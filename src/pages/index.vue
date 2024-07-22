3f5bec2088813e9f9cf569684197805862c131fb
IPANEMA2\lucia <github_luciad@exilium.com>
2024-07-21 18:13:40 -0700

    First commit kind of works

---
+++ src/pages/index.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { Round } from '@/types/player'
+
+const playerNames = ref(['Alice', 'Bob', 'Charlie'])
+
+const rounds = ref<Round[]>([
+  { round: 1, scores: [100, -20, 120] },
+  { round: 2, scores: [400, 50, 350] },
+  { round: 3, scores: [700, 300, 900] },
+  { round: 1, scores: [100, -20, 120] },
+  { round: 2, scores: [400, 50, 350] },
+  { round: 3, scores: [700, 300, 900] },
+  { round: 1, scores: [100, -20, 120] },
+  { round: 2, scores: [400, 50, 350] },
+  { round: 3, scores: [700, 300, 900] },
+  { round: 1, scores: [100, -20, 120] },
+  { round: 2, scores: [400, 50, 350] },
+  { round: 3, scores: [700, 300, 900] },
+  { round: 1, scores: [100, -20, 120] },
+  { round: 2, scores: [400, 50, 350] },
+  { round: 3, scores: [700, 300, 900] },
+  { round: 1, scores: [100, -20, 120] },
+  { round: 2, scores: [400, 50, 350] },
+  { round: 3, scores: [700, 300, 900] },
+])
+
+
+const emptyplayerNames = [] as string[]
+
+const emptyrounds = ref<Round[]>([
+])
+
+</script>
+
+<template>
+  <ScoreKeeper :players="emptyplayerNames" :rounds="emptyrounds"/>
+</template>
+
---
+++ src/types/player.ts
@@ -0,0 +1,5 @@
+export interface Round {
+    round: number
+    scores: number []
+  }
+
